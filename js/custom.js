06a1ea36472c927a4cc82e4a9debce4be3711924
asliabir <abiruzzaman.molla@gmail.com>
2020-09-13 20:53:13 +0600

    team section and work section

--- js/custom.js
+++ js/custom.js
@@ -3,3 +3,28 @@
 $(document).ready(function(){
     new WOW().init();
 });
+
+// Work popup
+
+$(document).ready(function(){
+    $("#popup-image").magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+            enabled: true,
+        }
+          
+    });
+});
+
+// Owl Carousel
+
+$(document).ready(function(){
+    $("#team-members").owlCarousel({
+        items: 3,
+        autoplay: true,
+        smartspeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
+});
